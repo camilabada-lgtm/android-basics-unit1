edb6559cf7796e5e16a2761891f2d3c14f538140
CamilaB <camila.bada@unmsm.edu.pe>
2026-08-30 19:40:10 -0500

    Reorganiza Ejercicio 1 y agrega ejercicios de practica 1-7

---
+++ Semana 1/Ejercicio1-primer-programa/ejercicios-practica.kt
@@ -0,0 +1,44 @@
+// Ejercicio 1
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+}
+
+// Ejercicio 2
+fun main() {
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+// Ejercicio 3
+fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+// Ejercicio 4
+fun main() {
+    println("Tomorrow is rainy")
+}
+
+// Ejercicio 5
+fun main() {
+    println("There is a chance of snow")
+}
+
+// Ejercicio 6
+fun main() {
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+}
+
+// Ejercicio 7
+fun main() {
+    println("How's the weather today?")
+}
